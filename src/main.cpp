56ea530c2f9ab5adcda6bf9e785fd4737d1c46ba
telmomm <miguelmedinatelmo@gmail.com>
2025-04-13 13:59:11 +0200

    Added keypad

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <ArduinoBLE.h>
 #include <U8g2lib.h>
 #include <Wire.h>
+#include <Keypad.h>
+
 
 // DEBUG MODE
 #define DEBUG
@@ -21,23 +23,60 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, RST_PIN, SCL_PIN, SDA_PIN);
 
 
 BLEService pulsadorService("19B10000-E8F2-537E-4F6C-D104768A1214");
-BLECharacteristic pulsadorCharacteristic("b444ea9a-a1b8-11ee-8c90-0242ac120002", BLENotify | BLERead, 12);
+BLECharacteristic pulsadorCharacteristic("b444ea9a-a1b8-11ee-8c90-0242ac120002", BLENotify | BLERead, 40);
 
-#define F1PIN 32
-#define F2PIN 33
-#define F3PIN 14
-#define VBATPIN 35
+#define F1PIN 22
+#define F2PIN 21
+#define F3PIN 19
+#define INPUT1PIN 32
+#define INPUT2PIN 33
+#define INPUT3PIN 27
+#define INPUT4PIN 14
+#define INPUT5PIN 12
+#define INPUT6PIN 13
+#define INPUT7PIN 15
+#define INPUT8PIN 4
+#define INPUT9PIN 5
+#define INPUT10PIN 18
 
-uint32_t F1, F2, F3;
+volatile uint32_t F1, F2, F3;
 
 uint32_t message[4];
 
 const uint32_t debounceTime = 200; // Tiempo de rebote en milisegundos
-uint32_t lastInterruptTimeF1 = 0;
-uint32_t lastInterruptTimeF2 = 0;
-uint32_t lastInterruptTimeF3 = 0;
+volatile uint32_t lastInterruptTimeF1 = 0;
+volatile uint32_t lastInterruptTimeF2 = 0;
+volatile uint32_t lastInterruptTimeF3 = 0;
 bool newInputValue = false;
-float batterySOC = 100;
+
+enum ButtonType {BOOL, TIMESTAMP};
+
+struct Button {
+  const char* name;
+  ButtonType type;
+  uint32_t timestamp;
+  bool pressed;
+};
+
+const byte rowsCount = 3;
+const byte columsCount = 3;
+
+String keyNames[rowsCount][columsCount] = {
+  {"Inicio", "Carrera", "Pausa"},
+  {"Fin", "6 Segundos", "4 Puntos"},
+  {"Reset", "Eliminado", "Extra"}
+};
+
+char keys[rowsCount][columsCount] = {
+  { 'I','C','P'},
+  { 'F','6','4'},
+  { 'R','E','X'}
+};
+
+byte rowPins[rowsCount] = {INPUT4PIN, INPUT5PIN, INPUT6PIN};  
+byte colPins[columsCount] = {INPUT1PIN, INPUT2PIN, INPUT3PIN}; 
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rowsCount, columsCount);
+
 
 void handleInterruptF1() {
   uint32_t currentMillis = millis();
@@ -65,7 +104,7 @@ void handleInterruptF3() {
     newInputValue = true;
   }
 }
-
+/*
 float readBatteryLevel(){
   uint32_t raw = analogRead(VBATPIN);
   float voltage = raw * 3.3 / 4095.0;
@@ -74,51 +113,39 @@ float readBatteryLevel(){
   float percentage = (voltage - minVoltage) / (maxVoltage - minVoltage) * 100;
   return percentage;
 }
+*/
 
-void initDisplay(){
-  DEBUG_PRINTLN("Init Display");
-  u8g2.begin();
-}
+//const uint32_t debounceTime = 200;
+uint32_t lastPressedTime[3][3] = {0};
+//bool newInputValue = false;
 
-void displayMessage(String message){
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_10x20_tf);
-
-
-  int screenHeight = u8g2.getDisplayHeight();
-  int textHeight = u8g2.getFontAscent() - u8g2.getFontDescent();
-  int yPos = (screenHeight - textHeight) / 2;
-
-  int screenWidth = u8g2.getDisplayWidth();
-  int textWidth = u8g2.getStrWidth(message.c_str());
-  int xPos = (screenWidth - textWidth) / 2;
-
-  u8g2.drawStr(xPos, yPos, message.c_str());
-  u8g2.sendBuffer();
-}
 
 void setup() {
   Serial.begin(115200);
-/*
+
   pinMode(F1PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(F1PIN), handleInterruptF1, RISING);
 
   pinMode(F2PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(F2PIN), handleInterruptF2, RISING);
 
-  //pinMode(F3PIN, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(F3PIN), handleInterruptF3, RISING);
+  pinMode(F3PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(F3PIN), handleInterruptF3, RISING);
 
   analogReadResolution(12); 
-  */
 
+  for (int i = 0; i < columsCount; i++) {
+    pinMode(colPins[i], INPUT_PULLUP);  // Estabiliza la lectura
+  }
+  
   // Inicializar el BLE
   if (!BLE.begin()) {
     Serial.println("No se pudo iniciar BLE");
     while (1);
   }
 
-  
+  pinMode(INPUT8PIN, INPUT);
+
   // Configurar el nombre del dispositivo BLE
   BLE.setLocalName("Kroner-Hub");
   BLE.setDeviceName("Kroner-Hub");
@@ -132,10 +159,40 @@ void setup() {
   BLE.setAdvertisingInterval(320); // 200 * 0.625 ms
   BLE.advertise();
 
-  initDisplay();
-  //displayMessage(BLE.address());
-  displayMessage("Kroner Hub");
 
+}
+
+void sendKeypadEvent(const String& name, uint32_t timestamp) {
+  char payload[50];  // Asegúrate de que sea lo suficientemente grande
+  snprintf(payload, sizeof(payload), "%s:%lu", name.c_str(), timestamp);
+
+  Serial.println(payload);
+
+  pulsadorCharacteristic.writeValue((uint8_t*)payload, strlen(payload));
+}
+
+void scanKeypad() {
+  char key = keypad.getKey();
+
+  if (key) {
+    uint32_t now = millis();
+
+    for (int i = 0; i < rowsCount; i++) {
+      for (int j = 0; j < columsCount; j++) {
+        if (keys[i][j] == key) {
+          if (now - lastPressedTime[i][j] > debounceTime) {
+            lastPressedTime[i][j] = now;
+
+            //Serial.print("Tecla presionada: ");
+            //Serial.println(keyNames[i][j]);
+
+            sendKeypadEvent(keyNames[i][j], now);
+            return;
+          }
+        }
+      }
+    }
+  }
 }
 
 
@@ -155,13 +212,15 @@ void loop(){
     Serial.println(central.connected());
     // Bucle para recibir comandos desde el cliente BLE
     while (central.connected()) {
+
+      scanKeypad();
+
       if (newInputValue){
-        batterySOC = readBatteryLevel();
         message[0] = F1;
         message[1] = F2;
         message[2] = F3;
         //message[3] = batterySOC;
-        message[3] = static_cast<uint32_t>(batterySOC * 100);
+        //message[3] = static_cast<uint32_t>(batterySOC * 100);
 
         Serial.print("F1: ");
         Serial.print(F1);
@@ -169,8 +228,6 @@ void loop(){
         Serial.print(F2);
         Serial.print(" - F3: ");
         Serial.print(F3);
-        Serial.print(" - Battery Level: ");
-        Serial.println(batterySOC);
         Serial.println();
 
         pulsadorCharacteristic.writeValue((uint8_t*)message, sizeof(message));
